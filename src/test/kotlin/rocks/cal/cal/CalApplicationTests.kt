2342da6aebb4534d08dd750b8304ff85e019be5d
Bendik Solheim <bendik@solheim.im>
2018-12-05 22:19:41 +0100

    Initial commit

---
+++ src/test/kotlin/rocks/cal/cal/CalApplicationTests.kt
@@ -0,0 +1,16 @@
+package rocks.cal.cal
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
