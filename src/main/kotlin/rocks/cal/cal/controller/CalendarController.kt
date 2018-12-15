45df3c88f1bc55d93e24c76ea86958be82c45c1d
Bendik Solheim <bendik@solheim.im>
2018-12-15 21:17:35 +0100

    Support multiple years through /{year}

--- src/main/kotlin/rocks/cal/cal/controller/CalendarController.kt
+++ src/main/kotlin/rocks/cal/cal/controller/CalendarController.kt
@@ -2,14 +2,22 @@ package rocks.cal.cal.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import rocks.cal.cal.calendar.CalendarService
 import rocks.cal.cal.html.calendarPage
+import java.time.LocalDate
 
 @RestController
 class CalendarController
 @Autowired constructor(private val calendarService: CalendarService){
 
     @GetMapping("/")
-    fun index(): String = calendarPage(calendarService.get(2018))
+    fun index(): String = calendarPage(calendarService.get(LocalDate.now().year))
+
+    @GetMapping("/{year}")
+    fun year(@PathVariable year: String) =
+            year.toIntOrNull()
+                ?.let {calendarPage(calendarService.get(it))}
+                ?: "$year is not a year, is it?"
 }
