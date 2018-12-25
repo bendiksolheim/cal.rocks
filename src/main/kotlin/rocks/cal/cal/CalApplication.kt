0880133cf571c7a1f9f9e6604f4b8dbe1a6ff302
Bendik Solheim <bendik@solheim.im>
2018-12-25 22:08:42 +0100

    Log default timezone on startup

--- src/main/kotlin/rocks/cal/cal/CalApplication.kt
+++ src/main/kotlin/rocks/cal/cal/CalApplication.kt
@@ -2,10 +2,12 @@ package rocks.cal.cal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class CalApplication
 
 fun main(args: Array<String>) {
     runApplication<CalApplication>(*args)
+    println(TimeZone.getDefault())
 }
