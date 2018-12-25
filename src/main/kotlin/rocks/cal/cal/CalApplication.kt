497299ec6665f10f06e62049646ee8847d2f62b1
Bendik Solheim <bendik@solheim.im>
2018-12-25 22:31:55 +0100

    Remove timezone/locale prints on startup

--- src/main/kotlin/rocks/cal/cal/CalApplication.kt
+++ src/main/kotlin/rocks/cal/cal/CalApplication.kt
@@ -2,12 +2,10 @@ package rocks.cal.cal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.util.*
 
 @SpringBootApplication
 class CalApplication
 
 fun main(args: Array<String>) {
     runApplication<CalApplication>(*args)
-    println(TimeZone.getDefault())
 }
