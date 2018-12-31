cdb0127ec034080c06664e1288a4b20caa2f1756
Bendik Solheim <bendik@solheim.im>
2018-12-31 21:53:22 +0100

    Force HTTPS on Heroku

---
+++ src/main/kotlin/rocks/cal/cal/WebSecurityConfig.kt
@@ -0,0 +1,16 @@
+package rocks.cal.cal
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.util.matcher.RequestMatcher
+
+@Configuration
+class WebSecurityConfig: WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.requiresChannel()
+                .requestMatchers(RequestMatcher { it.getHeader("X-Forwarded-Proto") != null })
+                .requiresSecure()
+    }
+}
