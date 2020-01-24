3ddad624246e7095698b789368ad801125ba3994
Bendik Solheim <hello@bendik.dev>
2020-01-24 19:42:50 +0100

    Force norwegian locale

--- src/main/kotlin/rocks/cal/cal/CalApplication.kt
+++ src/main/kotlin/rocks/cal/cal/CalApplication.kt
@@ -2,10 +2,12 @@ package rocks.cal.cal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class CalApplication
 
 fun main(args: Array<String>) {
+    Locale.setDefault(Locale("nb", "NO"))
     runApplication<CalApplication>(*args)
 }
