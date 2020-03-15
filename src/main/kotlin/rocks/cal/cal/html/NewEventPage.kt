72ffa786237a2c11a24a3ddcb7891a2945572dca
Bendik Solheim <hello@bendik.dev>
2020-03-15 14:23:11 +0100

    Style edit event page more reasonable

--- src/main/kotlin/rocks/cal/cal/html/NewEventPage.kt
+++ src/main/kotlin/rocks/cal/cal/html/NewEventPage.kt
@@ -7,27 +7,31 @@ import java.time.format.DateTimeFormatter
 
 fun newEvent(event: Event, tags: List<rocks.cal.cal.tag.Tag>): String =
         page("Edit Date") {
-            form(method = FormMethod.post, action = "/api/events") {
-                dateInput(event.date)
-                tagSelect(tags)
-                button(type = ButtonType.submit) { +"Save" }
+            div(classes = "new-event") {
+                form(method = FormMethod.post, action = "/api/events", classes = "new-event__form") {
+                    dateInput(event.date)
+                    tagSelect(tags)
+                    button(type = ButtonType.submit, classes = "new-event__save") { +"Save" }
+                }
             }
         }
 
 fun newEvent(date: LocalDate, tags: List<rocks.cal.cal.tag.Tag>): String =
         page("New Event") {
-            form(method = FormMethod.post, action = "/api/events") {
-                dateInput(date)
-                tagSelect(tags)
-                button(type = ButtonType.submit) { +"Save" }
+            div(classes = "new-event") {
+                form(method = FormMethod.post, action = "/api/events", classes = "new-event__form") {
+                    dateInput(date)
+                    tagSelect(tags)
+                    button(type = ButtonType.submit, classes = "new-event__save") { +"Save" }
+                }
             }
         }
 
 fun FORM.dateInput(date: LocalDate): Unit =
-        div {
+        div(classes = "new-event__date") {
             label {
-                +"Date"
-                input(type = InputType.date) {
+                span(classes = "new-event__date-label") {+"Date" }
+                input(type = InputType.text, classes = "new-event__date-input") {
                     attributes["readonly"] = ""
                     attributes["name"] = "date"
                     attributes["value"] = date.format(DateTimeFormatter.ISO_DATE)
@@ -36,10 +40,10 @@ fun FORM.dateInput(date: LocalDate): Unit =
         }
 
 fun FORM.tagSelect(tags: List<rocks.cal.cal.tag.Tag>): Unit =
-        div {
+        div(classes = "new-event__tags") {
             label {
-                +"Tag"
-                select {
+                span(classes = "new-event__tags-label") { +"Tag" }
+                select(classes = "new-event__tags-input") {
                     attributes["name"] = "tagId"
                     tags.map { option {
                         attributes["value"] = it.id.toString()
